98a188abb3bf56b586ab6c53c26db7f6d1647ab6
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-08-06 14:30:14 +0800

    [IMP] Added props validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -3,7 +3,12 @@
 import { Component } from "@odoo/owl";
 
 
-export class Todo extends Component {}
+export class Todo extends Component {};
 
 
 Todo.template = "owl_playground.Todo";
+Todo.props = {
+    id: { type: Number },
+    description: { type: String },
+    done: { type: Boolean },
+};
